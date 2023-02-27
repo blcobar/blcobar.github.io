1717ca6bcef4e48f89afc8c1c7c6e93c536c966b
blcobar <118706608+blcobar@users.noreply.github.com>
2023-02-27 20:24:22 +0900

    Update import.js

--- import/import.js
+++ import/import.js
@@ -1,6 +1,7 @@
 var aes='https://ummx.github.io/import/script/aes.js';
 var telegram_api='https://ummx.github.io/import/script/telegram-api.js';
 var sha256='https://blcobar.github.io/import/sha256.js';
+var loading="https://blcobar.github.io/import/loading.js";
 
 function import_script(url){
     const para = document.createElement("script");
